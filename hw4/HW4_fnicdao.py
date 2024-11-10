282956629aadb0165fd32a7e75724439e71247a5
FionaN <fiona1941nicdao@gmail.com>
2024-11-09 21:04:34 -0600

    finished part 1 of hw4 and change dataset

--- hw4/HW4_fnicdao.py
+++ hw4/HW4_fnicdao.py
@@ -3,7 +3,7 @@
     Homework 4: Chapter 6 model selection
     PLEASE LOOK AT PDF FOR REPORT on SAKI 
 
-    USED a new dataset:  Customer Personality Analysis
+    used Titanic dataset:  
     Link: https://www.kaggle.com/datasets/imakash3011/customer-personality-analysis?resource=download
 
     PART 1 - 4  
@@ -14,69 +14,94 @@ from sklearn.model_selection import train_test_split # type: ignore
 from sklearn.linear_model import LogisticRegression # type: ignore
 from sklearn.metrics import f1_score # type: ignore
 
-"""
-part 1: randomly split the data into training (80%) and test (20%) sets
-"""
-
-# import marketing_campaign.csv file 
-## please change the file_path to where you stored the dataset marketing_campaign.csv file 
-file_path = '/Users/fionanicdao/loyola/machineLearning/hw4/marketing_campaign.csv'
-df = pd.read_csv(file_path, sep="\t")
-# print(df.info())
-
-class MarketingCampaignDataset(object):
+#import Titanic train.csv file 
+## please change the file_path to where you store titanic train.csv file
+file_path = '/Users/fionanicdao/loyola/machineLearning/titanic_hw1/train.csv'
+df = pd.read_csv(file_path)
+#prepressing the Titanic Dataset 
+class TitanicDataset(object):
     def process_data(df):
-        process_df = df
-        process_df.isnull().sum(axis = 0)
-        # there are 24 Nan values in the income column so replace the Nan with the median inccome
-        
-        median = process_df['Income'].median()
-        process_df = process_df.fillna(median)
-        # print(process_df.isnull().sum(axis = 0))
-        # unique = process_df["Marital_Status"].unique()
-        # print(unique)
-        # education : Basic = 0, Graduation = 1, Master = 2, 2n Cycle = 2, PhD = 3
-        process_df["Education"] = process_df["Education"].map({'Basic':0, 'Graduation':1,'Master':2, '2n Cycle':2, 'PhD':3})
-        
-        process_df["Marital_Status"] = process_df["Marital_Status"].map({'Single':1, 'Together':2, 'Married':3, 'Divorced':0, 'Widow':4, 'Alone':1, 'Absurd':1,'YOLO':1 })
-        
-        process_df.drop('Dt_Customer', axis=1, inplace=True)
-        return process_df
-        
+        # drop useless data : PassengerId , Ticket , Fare , Cabin , Embarked
+        df = df.drop(["PassengerId", "Ticket", "Fare", "Cabin", "Embarked","Name","Age"], axis=1)
 
-df = MarketingCampaignDataset.process_data(df)
-print(df)
-y = df["Response"].values
-# print(y)
-X = df.iloc[0:,0:28].values
-# print(X)
+        df["Survived"] = df["Survived"].map({0 : -1, 1:1})
+        # no processing done with Pclass
+         
+        # sex : male = 0 and female = 1
+        df["Sex"] = df["Sex"].map({"male": 0, "female": 1})
 
-X_train, X_test,y_train, y_test = train_test_split(X,y, test_size=0.20,random_state=42)
+        # parch 
+        def parch_separate(passenger):
+            parch = passenger
+            if( parch > 0) : # if passenger have a parent / children  = 1
+                return 1
+            else : # else passenger has no parent / children = 0 
+                return 0
+            
+        df["Parch"] = df["Parch"].apply(parch_separate)
 
-# performance metric choosen is F1 score because of imbalance reponse (y) there are more 0 than 1 
+        # sibsp
+        def sibsp_separate(passenger):
+            sibsp = passenger
+            if(sibsp > 0): # if passengers has a sibling / spouse = 1
+                return 1
+            else: # passenger has no parent / children = 0
+                return 0
+
+        df["SibSp"] = df["SibSp"].apply(sibsp_separate)
+
+        return df
+    
+df = TitanicDataset.process_data(df)
+# set X(features) and y (target class labels) from dataset
+y = df["Survived"].values
+X = df.iloc[0:,1:5].values
+# df_training (70%), df_development (15%), df_test (15%)
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.20, random_state=42)
 
 """
 part 2: train and evaluate a classifer of your choice (e.g. logistic regression, SVM) using n-fold cross validation
 """
-lr = LogisticRegression()
-lr.fit(X_train, y_train)
-weights = lr.coef_
-intercept = lr.intercept_
-print("part 1: LogisticRegression weights= ", weights) 
-print("part 1: LogisticRegression intercept= ",intercept)
-accuracy_train = lr.score(X_train, y_train)
-print("part 1: LogisticRegression accuracy = ", accuracy_train)
-y_pred = lr.predict(X_train)
-f1 = f1_score(y_train, y_pred, average="weighted")
-print("part 1: LogisticRegression f1 score: ", f1)
+# lr = LogisticRegression()
+# lr.fit(X_train, y_train)
+# weights = lr.coef_
+# intercept = lr.intercept_
+# print("part 1: LogisticRegression weights= ", weights)
+# print("part 1: LogisticRegression intercept= ",intercept)
+# accuracy_train = lr.score(X_test, y_test)
+# print("part 1: LogisticRegression accuracy = ", accuracy_train)
+# y_pred = lr.predict(X_test)
+# f1 = f1_score(y_test, y_pred, average="weighted")
+# print("part 1: LogisticRegression f1 score: ", f1)
 
 # n fold cross validation : try with n = 5 
-n =5;
+n =10;
 # divide the training data into 5 groups 
+num_examples = len(X_train)
+fold_size = num_examples // n 
 
-"""
-part 3: implement your own grid search procedure from scrach which should include a search over at least two hyper-parameters
-    C 
-    penalty parameters
+indices = np.random.permutation(num_examples)
+
+f1_scores = []
+
+for i in range(n):
+    start, end = i * fold_size, (i+1) * fold_size
+    test_indices = indices[start:end]
     
-"""
+    X_ntest, y_ntest = X_train[test_indices],y_train[test_indices]
+    train_indices = np.concatenate([indices[:start],indices[end:]])
+    X_ntrain, y_ntrain = X_train[train_indices],y_train[train_indices]
+    
+    model = LogisticRegression(max_iter=200)
+    model.fit(X_ntrain, y_ntrain)
+    
+    y_pred = model.predict(X_ntest)
+    accuracy = f1_score(y_ntest, y_pred)
+    f1_scores.append(accuracy)
+    print(f"Fold {i +1} accuracy : {accuracy:.4f}")
+    
+mean_accuracy = np.mean(f1_scores)
+std_accuracy = np.std(f1_scores)
+print(f"\nMean accuracy: {mean_accuracy:.4f}") 
+print(f"Standard deviation of accuracy: {std_accuracy:.4f}")
+
