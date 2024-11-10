74091aba0ed357641300a028842803bc755accaf
FionaN <fiona1941nicdao@gmail.com>
2024-11-10 16:44:35 -0600

    add finished part 4 to hw4

--- hw4/HW4_fnicdao.py
+++ hw4/HW4_fnicdao.py
@@ -62,18 +62,8 @@ X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.20, random_
 """
 part 2: train and evaluate a classifer of your choice (e.g. logistic regression, SVM) using n-fold cross validation
 """
-# lr = LogisticRegression()
-# lr.fit(X_train, y_train)
-# weights = lr.coef_
-# intercept = lr.intercept_
-# print("part 1: LogisticRegression weights= ", weights)
-# print("part 1: LogisticRegression intercept= ",intercept)
-# accuracy_train = lr.score(X_test, y_test)
-# print("part 1: LogisticRegression accuracy = ", accuracy_train)
-# y_pred = lr.predict(X_test)
-# f1 = f1_score(y_test, y_pred, average="weighted")
-# print("part 1: LogisticRegression f1 score: ", f1)
-def n_fold_cross_validation(X, y, n=5) : 
+
+def n_fold_cross_validation(X, y, n=5, C=1.0, penalty='l2', solver='lbfgs') : 
     # n fold cross validation : try with n = 5 
     
     # divide the training data into 5 groups 
@@ -92,7 +82,7 @@ def n_fold_cross_validation(X, y, n=5) :
         train_indices = np.concatenate([indices[:start],indices[end:]])
         X_ntrain, y_ntrain = X[train_indices],y[train_indices]
         
-        lr = LogisticRegression(max_iter=200)
+        lr = LogisticRegression(C=C, penalty=penalty, solver=solver, max_iter=1000)
         lr.fit(X_ntrain, y_ntrain)
         
         y_pred = lr.predict(X_ntest)
@@ -104,6 +94,8 @@ def n_fold_cross_validation(X, y, n=5) :
     std_accuracy = np.std(f1_scores)
     print(f"\nMean accuracy: {mean_accuracy:.4f}") 
     print(f"Standard deviation of accuracy: {std_accuracy:.4f}")
+    
+    return(mean_accuracy, C, penalty, lr)
 
 n_fold_cross_validation(X_train,y_train,10)
 """
@@ -111,8 +103,8 @@ part 3: implement your own grid search procedure from scratch which should inclu
 run a grid search over these hyperparameters and decide which hyperparameter combination gives you the best performance 
 your grid search should rely on your n-fold cross validation implementation from subproblem 3 above the score different models 
 """
-
-def grid_search_C_and_penalty(X,y,X_test, y_test) :
+print("### part 3 ###")
+def grid_search_C_and_penalty(X,y) :
     C_values = [0.01, 0.1, 1, 10, 100]
     penalty_values = ['l1', 'l2']
     
@@ -126,24 +118,32 @@ def grid_search_C_and_penalty(X,y,X_test, y_test) :
                 solver = 'liblinear' # supports l1 regularization
             else : solver = 'lbfgs' # supports l2 regularization
             
-            lr = LogisticRegression(C=C, penalty=penatly, solver=solver, max_iter=1000)
-            lr.fit(X,y)
+            f1, C, penatly, lr = n_fold_cross_validation(X,y,10,C, penatly,solver)
             
-            y_pred = lr.predict(X_test)
-            f1 = f1_score(y_test, y_pred)
-            results.append({'C': C, 'penalty': penatly,'f1':f1})
+            # lr = LogisticRegression(C=C, penalty=penatly, solver=solver, max_iter=1000)
+            # lr.fit(X,y)
+            
+            # y_pred = lr.predict(X_test)
+            # f1 = f1_score(y_test, y_pred)
+            results.append({'C': C, 'penalty': penatly, 'f1':f1})
             
             if f1 > best_f1_score :
                 best_f1_score = f1
-                best_params = {'C': C, 'penalty':penatly}
+                best_params = {'C': C, 'penalty':penatly, 'solver':solver}
     # Display the results
+    print("\n\n")
     print("Best Parameters:", best_params)
     print("Best Validation F1 Score:", best_f1_score)
     print("\nAll Results:")
     for result in results:
         print(result)
+    return(best_params['C'], best_params['penalty'], best_params['solver'],best_f1_score)
 
-grid_search_C_and_penalty(X_train, y_train, X_test, y_test)
+best_C, best_penatly,best_solver, best_f1 = grid_search_C_and_penalty(X_train, y_train)
+print(best_C)
+print(best_penatly)
+print(best_solver)
+print(best_f1)
 
 """
 PART 4 
@@ -151,4 +151,10 @@ evaluate the best model that you identify in part 3 and report
 its performance on the test set. 
 compare this number to the performance of your best model on the training set(i.e. train and test on the training data) and explain the difference 
 """
-# need to finish ! 
+
+lr = LogisticRegression(C=best_C, penalty=best_penatly, solver=best_solver, max_iter=1000)
+lr.fit(X_test,y_test)
+            
+y_pred = lr.predict(X_test)
+f1 = f1_score(y_test, y_pred)
+print("test f1 score",f1)
